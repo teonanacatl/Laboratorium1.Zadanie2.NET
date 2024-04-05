1679fdf671bf5ec2ac78d4713bc2e27a7acbeee9
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-04-05 19:50:51 +0200

    Restoration of the starting point

--- Laboratorium1.Zadanie2.NET/Program.cs
+++ Laboratorium1.Zadanie2.NET/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Laboratorium1.Zadanie2.NET
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Samochod s1 = Samochod.Stworz();
+            s1.WypiszInfo();
+            s1.Marka = "Fiat";
+            s1.Model = "126p";
+            s1.IloscDrzwi = 2;
+            s1.PojemnoscSilnika = 650;
+            s1.SrednieSpalanie = 6.0;
+            s1.WypiszInfo();
 
-Console.WriteLine("Hello, World!");
+            Samochod s2 = Samochod.Stworz("Syrena", "105", 2, 800, 7.6);
+            s2.WypiszInfo();
+
+            double kosztPrzejazdu = s2.ObliczKosztPrzejazdu(30.5, 4.85);
+            Console.WriteLine("Koszt przejazdu: " + kosztPrzejazdu);
+
+            Samochod.WypiszLiczbeSamochodow();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratorium1.Zadanie2.NET/Samochod.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+namespace Laboratorium1.Zadanie2.NET;
+
+public class Samochod
+{
+    public string Marka { get; set; }
+    public string Model { get; set; }
+    public int IloscDrzwi { get; set; }
+    public int PojemnoscSilnika { get; set; }
+    public double SrednieSpalanie { get; set; }
+    private static int _liczbaSamochodow;
+    
+    private Samochod()
+    {
+        Marka = "Nieznana";
+        Model = "Nieznany";
+        IloscDrzwi = 0;
+        PojemnoscSilnika = 0;
+        SrednieSpalanie = 0.0;
+    }
+    
+    private Samochod(string marka, string model, int iloscDrzwi, int pojemnoscSilnika, double srednieSpalanie)
+    {
+        Marka = marka;
+        Model = model;
+        IloscDrzwi = iloscDrzwi;
+        PojemnoscSilnika = pojemnoscSilnika;
+        SrednieSpalanie = srednieSpalanie;
+    }
+    
+    public static Samochod Stworz()
+    {
+        _liczbaSamochodow++;
+        return new Samochod();
+    }
+
+    public static Samochod Stworz(string marka, string model, int iloscDrzwi, int pojemnoscSilnika, double srednieSpalanie)
+    {
+        _liczbaSamochodow++;
+        return new Samochod(marka, model, iloscDrzwi, pojemnoscSilnika, srednieSpalanie);
+    }
+    
+    private double ObliczSpalanie(double dlugoscTrasy)
+    {
+        double spalanie = (SrednieSpalanie * dlugoscTrasy) / 100.0;
+        return spalanie;
+    }
+
+    public double ObliczKosztPrzejazdu(double dlugoscTrasy, double cenaPaliwa)
+    {
+        double spalanie = ObliczSpalanie(dlugoscTrasy);
+        double kosztPrzejazdu = spalanie * cenaPaliwa;
+        return kosztPrzejazdu;
+    }
+    
+    public void WypiszInfo()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine($"Marka: {Marka}");
+        stringBuilder.AppendLine($"Model: {Model}");
+        stringBuilder.AppendLine($"Ilość drzwi: {IloscDrzwi}");
+        stringBuilder.AppendLine($"Pojemność silnika: {PojemnoscSilnika}");
+        stringBuilder.AppendLine($"Średnie spalanie: {SrednieSpalanie}");
+        Console.WriteLine(stringBuilder.ToString());
+    }
+    
+    public static void WypiszLiczbeSamochodow()
+    {
+        Console.WriteLine($"Liczba samochodów: {_liczbaSamochodow}");
+    }
+}
